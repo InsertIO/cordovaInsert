cde7de46ad6b6906175f6fdb10664527c52a4399
Tomer Levinson <tomer@insert.io>
2016-12-01 17:10:12 +0200

    Android stuff.

---
+++ src/android/Insert.java
@@ -0,0 +1,11 @@
+package com.cordova.insert.plugin;
+import org.apache.cordova.*;
+import org.apache.cordova.PluginResult.Status;
+
+
+public class Insert extends CordovaPlugin {
+	@override
+	public boolean execute(String action, JSONArray inputs, CallbackContext callbackContext) throws JSONException {
+		
+	}
+}
