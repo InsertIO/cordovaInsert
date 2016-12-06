8e080a58c097bc4e5d0ae849634cc7be98977589
Tomer Levinson <tomer@insert.io>
2016-12-06 11:48:42 +0200

    Added switch-case

--- src/android/Insert.java
+++ src/android/Insert.java
@@ -4,11 +4,21 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import sdk.insert.io.Insert;
+import static sdk.insert.io.Insert.dismissVisibleInserts;
 
 public class Insert extends CordovaPlugin {
 	@Override
 	public boolean execute(String action, JSONArray inputs, CallbackContext callbackContext) throws JSONException {
+		switch (action) {
+			case "dismissVisibleInserts":
+				dismissVisibleInserts();
+				break;
+			case "eventOccured":
+				// eventOccured(inputs.getString(0), inputs.getJSONObject(0));
+				break;
+			default:
+				break;
+		}
 		return true;
 	}
 }
