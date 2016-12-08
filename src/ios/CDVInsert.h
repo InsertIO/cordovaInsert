34df3655c117de0af9ade874f074ddef616e43d5
netanel <netanel@insert.io>
2016-12-08 23:03:55 +0200

    update API

--- src/ios/CDVInsert.h
+++ src/ios/CDVInsert.h
@@ -5,4 +5,5 @@
 
 @interface CDVInsert : CDVPlugin
 - (void)dismissVisibleInserts:(CDVInvokedUrlCommand *)command;
+- (void)eventOccured:(CDVInvokedUrlCommand *)command;
 @end
--- src/ios/CDVInsert.m
+++ src/ios/CDVInsert.m
@@ -17,19 +17,18 @@
 }
 
 
-// Need to add this api at the end of sprint-39
-
-//-(void)eventOccured:(CDVInvokedUrlCommand *)command {
-//    vCDVPluginResult *pluginResult;
-//    NSString *callbackId = command.callbackId;
-//    if (command.arguments.count >= 2) {
-//        NSString *actionName = [command argumentArIndex:0];
-//        NSDictionary *params = [command argumentArIndex: 1];
-//        [InsertManager sharedManager eventOccured:actionName json:params];
-//        [self sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] to:command.callbackId];
-//    } else {
-//        [self sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"invalid arguments"] to:command.callbackId];
-//}
+-(void)eventOccured:(CDVInvokedUrlCommand *)command {
+    CDVPluginResult *pluginResult;
+    NSString *callbackId = command.callbackId;
+    if (command.arguments.count >= 2) {
+        NSString *actionName = command.arguments[0];
+        NSDictionary *params = command.arguments[1];
+        [[InsertManager sharedManager] eventOccured:actionName parameters:params];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+    } else {
+        
+    }
+}
 
 
 
