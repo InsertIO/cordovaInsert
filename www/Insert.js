fdaf740235d9337f1560a066abcff9bfb7872327
Tomer Levinson <tomer@insert.io>
2017-11-05 10:04:23 +0200

    Removed setUserId

--- www/Insert.js
+++ www/Insert.js
@@ -117,24 +117,5 @@ module.exports = {
       "setUserAttributes",
       [ensureStringValues(attributes)]
     );
-  },
-
-  /**
-   * Set a user id to tailor inserts to users.
-   * 
-   * @param {string} userId - an application defined user id
-   * @param {function} [success] - called when the id was set
-   * @param {function} [error] - called when the id could not be set
-   * 
-   * @see [How to: Use custom data when defining an Insert Audience](https://support.insert.io/hc/en-us/articles/207569209-How-to-Use-custom-data-when-defining-an-Insert-Audience)
-   */
-  setUserId: function(userId, success, error) {
-    cordova.exec(
-      success,
-      error,
-      "Insert",
-      "setUserId",
-      [userId]
-    );
   }
 };
